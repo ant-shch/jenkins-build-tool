04d2358e69d1d46cf58a4586a064b78e9151c194
Anton Shcherbyna <khdevnet@gmail.com>
2017-05-12 18:30:01 +0300

    Delete nunitrun.bat

--- nunitrun.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd ..
-FOR /D /r %%G in ("*.Tests") DO (
-    FOR /D %%T in ("%%G\bin\*") DO (
-	    For /F "tokens=*" %%F IN ('dir /b /s %%T\*.Tests.dll') DO (
-		   nunit3-console.exe %%F
-	    )))
