8b6374b2f1d67ad1d874435a2389463ea0ba7702
Anton Shcherbyna <khdevnet@gmail.com>
2017-05-13 14:21:47 +0300

    Update JsonConfiguration.groovy

--- JsonConfiguration.groovy
+++ JsonConfiguration.groovy
@@ -1,10 +1,33 @@
 import groovy.json.JsonSlurper
 
-class JsonConfiguration {
-   def Read(def path) { 
+class Repository {
+      Repository(name,url)
+      {
+         this.Name = name
+         this.Url = url
+      }
+      def Name
+      def Url
+}
+
+class Configuration {
+      Configuration(repositories) {          
+        this.Repositories = repositories
+      }
+   
+      List<Repository> Repositories   
+}
+
+class JsonConfigurationRepository {
+   Configuration Get(def path) { 
          def configurationFile = new File(path)
-         return new JsonSlurper().parseText(configurationFile.text) 
+         def object = new JsonSlurper().parseText(configurationFile.text);
+         List<Repository> repositories = new ArrayList<Repository>();
+         for(def rep : object.repositories)  {
+             repositories.add(new Repository(rep.name, rep.url))
+         }
+         return new Configuration(repositories) 
        }
 }
 
-return new JsonConfiguration();
+return new JsonConfigurationRepository();
