7c2dc062afc4bd06b7a5a89e55653313c95ea0d8
Anton Shcherbyna <khdevnet@gmail.com>
2017-05-13 13:35:28 +0300

    Update JsonConfiguration.groovy

--- JsonConfiguration.groovy
+++ JsonConfiguration.groovy
@@ -3,7 +3,7 @@ import groovy.json.JsonSlurper
 class JsonConfiguration {
    def Read(def path) { 
          def configurationFile = new File(path)
-         return new JsonSlurper().parseText(inputFile.text) 
+         return new JsonSlurper().parseText(configurationFile.text) 
        }
 }
 
